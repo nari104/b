26663473399ea93f6cc3e4077e35b754cbc048a3
nari104 <172471497+nari104@users.noreply.github.com>
2024-07-22 22:27:48 +0000

    Bookers1 1-comnit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+      if @book.save
+      redirect_to '/books/new'
+    else
+      render :new
+    end
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+      redirect_to books_path(book.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
